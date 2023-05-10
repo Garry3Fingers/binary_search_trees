952747d1cc28af5d8d9202ebda170b950ffda17f
Garry3Fingers <www.vlad78@yandex.ru>
2023-05-10 21:38:48 +0300

    Create class Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,1 +1,16 @@
+# frozen_string_literal: true
 
+require_relative 'node'
+
+# This class represents the full tree
+class Tree
+  attr_reader :array, :root_node
+
+  def initialize(array)
+    @array = array
+    @root_node = nil
+  end
+end
+
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+p tree
