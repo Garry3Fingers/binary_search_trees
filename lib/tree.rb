ff6ddee304b9f0368cfcc38c648ddecccae7f1bb
Garry3Fingers <www.vlad78@yandex.ru>
2023-05-17 21:58:26 +0300

    Add #inorder method

--- lib/tree.rb
+++ lib/tree.rb
@@ -82,6 +82,19 @@ class Tree
     arr unless arr.empty?
   end
 
+  def inorder(root = root_node, arr = [], &block)
+    inorder(root.left_child, arr, &block) unless root.left_child.nil?
+
+    if block_given?
+      block.call(root)
+    else
+      arr.push(root.value)
+    end
+
+    inorder(root.right_child, arr, &block) unless root.right_child.nil?
+    arr unless arr.empty?
+  end
+
   # This method from https://www.theodinproject.com/lessons/ruby-binary-search-trees
   def pretty_print(node = root_node, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
@@ -119,7 +132,7 @@ end
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.build_tree
 tree.pretty_print
-tree.preorder { |node| puts node.value }
+p tree.inorder
 # tree.level_order { |node| puts node.value }
 # # tree.insert(100)
 # tree.insert(200)
